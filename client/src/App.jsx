64c79a60f8e3c934744cfff854f5f12ac4d15998
Brook Tinnapat <53811329+708799@users.noreply.github.com>
2024-06-07 13:38:05 +0700

    Assignment : React Data Fetching Products Assignment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,53 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const [products, setProduct] = useState([]);
+  const getProducts = async () => {
+    const result = await axios.get("http://localhost:4001/products");
+    setProduct(result.data.data);
+  };
+  const removeProduct = (productIndex) => {
+    const deleteProduct = [...products];
+    deleteProduct.splice(productIndex, 1);
+    setProduct(deleteProduct);
+  };
+
   return (
     <div className="App">
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
       </div>
       <div className="product-list">
-        <div className="product">
-          <div className="product-preview">
-            <img
-              src="https://via.placeholder.com/350/350"
-              alt="some product"
-              width="350"
-              height="350"
-            />
-          </div>
-          <div className="product-detail">
-            <h1>Product name: ...</h1>
-            <h2>Product price: ... Baht</h2>
-            <p>Product description: .....</p>
-          </div>
+        {products.map((product, index) => (
+          <div className="product" key={index}>
+            <div className="product-preview">
+              <img
+                src="https://via.placeholder.com/350/350"
+                alt="some product"
+                width="350"
+                height="350"
+              />
+            </div>
+            <div className="product-detail">
+              <h1>Product name: {product.name}</h1>
+              <h2>Product price: {product.price} Baht</h2>
+              <p>Product description: {product.description}</p>
+            </div>
 
-          <button className="delete-button">x</button>
-        </div>
+            <button
+              className="delete-button"
+              onClick={() => removeProduct(index)}
+            >
+              x
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
